17985efb4ad21774c8d1d6ca466adf2464823afd
fst <fstasel@gmail.com>
2026-04-17 07:31:53 +0000

    Optimize int_sqrt example

--- examples/int_sqrt.cpp
+++ examples/int_sqrt.cpp
@@ -9,15 +9,16 @@ using namespace std;
 // Binary restoring square root algorithm
 template <typename T> T isqrt(T N) {
     T x = 0;
-    T s = 0;
+    T s = 0, sn;
     int msb = (N.getSize() >> 1) - 1;
     for (int k = msb; k >= 0; --k) {
         uint64_t delta = (1ULL << k);
         T diff = (x << (k + 1)) + (1ULL << (2 * k));
-        BinaryDigit c = (s + diff <= N).getData()[0];
-        T mask = Einteger(vector<BinaryDigit>(N.getSize(), c), false);
+        Ebool c = ((sn = s + diff) <= N);
+        BinaryDigit c0 = c.getData()[0];
+        T mask = Einteger(vector<BinaryDigit>(N.getSize(), c0), false);
         x += mask & delta;
-        s += mask & diff;
+        Eif(c) s = sn;
     }
     return x;
 }
